6bc0e421dbed8cf1f8643653c6c0c9f684f7055d
Fellipe Lopes <fellipelopessilva5@gmail.com>
2024-01-25 18:37:04 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,89 @@
+/*
+let titulo = document.querySelector('h1');
+                    //case sensitive: diferencia maiúsculas e minúsculas
+titulo.innerHTML = 'Jogo do número secreto';
+*/
+/*
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+*/
+
+/*
+let campo = document.querySelector(tag)
+campo.innerHTML = texto;
+//evite a repetição de código com funções
+*/
+
+//array.length - serve para ver a quantidade de objetos tem dentro do array
+//para pegar algums elemento dentro da lista se deve colocar o indice em que ele se encontra
+
+//Se entende na ordem dos comandos
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+exibirMensagemInicial();
+push
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled'); //id = identificar único
+    }else{
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        }else{
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+//uma function é um trecho de código que é responsável por uma determinada ação
+//deve ter o mesmo nome, no HTML e no javascript
+
+function gerarNumeroAleatorio(){
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeElementosLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeElementosLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        //lista.push - adiciona um elemento ao final da lista
+        //lista.pop - remove o último elemento
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disable', true);
+}
